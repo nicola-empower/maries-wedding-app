529d90f65b36fe80b56579bfb80420012b89dc4c
Nicola Berry <nicola@empowervaservices.co.uk>
2025-08-18 15:42:02 +0100

    update event schedule

--- src/components/SchedulePage.jsx
+++ src/components/SchedulePage.jsx
@@ -1,4 +1,4 @@
-import './SchedulePage.css'; // We'll create this file next for styling
+import './SchedulePage.css'; // I'll create this file next for styling
 
 function SchedulePage() {
   return (
@@ -6,8 +6,8 @@ function SchedulePage() {
       <h1>Today's Schedule</h1>
       <div className="schedule-timeline">
         <div className="schedule-item">
-          <p className="time">1:30 PM</p>
-          <p className="event">Guests Arrive & Welcome Drinks</p>
+          <p className="time">1:00 - 1:30 PM</p>
+          <p className="event">Guests Arrive</p>
         </div>
         <div className="schedule-item">
           <p className="time">2:00 PM</p>
@@ -15,23 +15,31 @@ function SchedulePage() {
         </div>
         <div className="schedule-item">
           <p className="time">2:45 PM</p>
-          <p className="event">Confetti & Cocktails</p>
+          <p className="event">Bridal Photos</p>
+        </div>
+         <div className="schedule-item">
+          <p className="time">4:30 PM</p>
+          <p className="event">Guests Seated for Meal</p>
         </div>
          <div className="schedule-item">
           <p className="time">5:00 PM</p>
           <p className="event">Speeches & Toasts</p>
         </div>
         <div className="schedule-item">
-          <p className="time">7:30 PM</p>
-          <p className="event">Cutting of the Cake</p>
+          <p className="time">6:30 PM</p>
+          <p className="event">Meal Ends</p>
         </div>
         <div className="schedule-item">
-          <p className="time">7:45 PM</p>
-          <p className="event">The First Dance</p>
+          <p className="time">7:30 PM</p>
+          <p className="event">Evening Guests Arrive</p>
         </div>
          <div className="schedule-item">
-          <p className="time">11:00 PM</p>
-          <p className="event">Carriages</p>
+          <p className="time">8:00 PM</p>
+          <p className="event">First Dance</p>
+        </div>
+        <div className="schedule-item">
+          <p className="time">12:00 AM</p>
+          <p className="event">Ends</p>
         </div>
       </div>
     </div>
